afa63aa0ce2e49edb05283eb80b27270826bca13
Zackary_Griffin <zackarygriffin30@gmail.com>
2020-06-27 16:37:02 -0400

    added basic menu bar

--- Visualizer.py
+++ Visualizer.py
@@ -3,18 +3,30 @@ import node as n
 
 #setting up window
 window = tk.Tk()
+window.title("Visualizer")
 WIDTH = 750
 HEIGHT = 750
 #creating list to hold all nodes
-node_list = [list(range(WIDTH//25)) for i in range(HEIGHT//25)]
+node_size = 25
+node_list = [list(range(WIDTH//node_size)) for i in range(HEIGHT//node_size)]
 #setting up canvas
 canvas = tk.Canvas(window, height=HEIGHT, width=WIDTH)
 canvas.pack()
 
+def create_menu():
+    #create menu object
+    menu = tk.Menu(window)
+    window.config(menu=menu)
+    #create file object
+    file = tk.Menu(menu)
+    #adding commands to file object
+    file.add_command(label='Set Start and Finish', command=set_Start_and_Finish)
+    file.add_command(label='Exit', command=client_exit)
+    #adding 'file' to menu
+    menu.add_cascade(label='File', menu=file)
+
+
 def draw_nodes(event=None):
-    #width and height for node
-    node_size = 25
-    
     i = 0
     #fills window with nodes at intervales of the square size
     for y in range(0, HEIGHT, node_size):
@@ -37,20 +49,31 @@ def draw_wall(event):
     y = event.y // 25
     node = node_list[x][y]
 
+    #if not start or finish node then draw wall
     if not node.is_start:
         if not node.is_finish:
+            #set to wall
             node.is_wall = True
             #change color of node to black
             canvas.itemconfig(node.node, fill='black')
+            
+#exit the app
+def client_exit():
+    exit()
 
+def set_Start_and_Finish():
+    pass
 
 def main():
     canvas.tag_bind("node", "<B1-Motion>", draw_wall)
     draw_nodes()
+    create_menu()
+
+
+
 
     window.mainloop()
-
-
+    
 
 
 main()
