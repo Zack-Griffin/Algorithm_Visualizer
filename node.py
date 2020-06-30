2e0ba1ca457b6e9af6ae32c8f91770525af63f66
Zack <zackarygriffin30@gmail.com>
2020-06-29 23:21:13 -0400

    removed unused code

--- node.py
+++ node.py
@@ -10,22 +10,9 @@ class Node:
         self.is_wall = False
         #holds the data returned from the canvas drawing
         self.node = None
-        #info for a-star
+        #info for pathfinding
         self.move_cost = 0
         self.estimate_cost = 0
         self.sum_cost = 0
         self.parent_node = None
         self.is_searched = False
-
-    def copy(self, node):
-        self.x = node.x
-        self.y = node.y
-        self.is_start = node.is_start
-        self.is_finish = node.is_finish
-        self.is_wall = node.is_wall
-        self.node = node.node
-        self.move_cost = node.move_cost
-        self.estimate_cost = node.estimate_cost
-        self.sum_cost = node.sum_cost
-        self.parent_node = node.parent_node
-        self.is_searched = node.is_searched
